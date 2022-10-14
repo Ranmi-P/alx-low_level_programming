c9dea8b3d115922cbe734360978bb0e5e5107b41
Oreoluwa Aremu <aremu.oluwaranmilowo@gmail.com>
2022-10-14 09:36:19 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Prints out the last part of a quote in the standard error
  *
- * Return: Always 0 (Success)
+ * Return: 1 if success
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
