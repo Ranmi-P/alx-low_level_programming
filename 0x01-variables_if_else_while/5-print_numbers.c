ef826b674b371e8b3d653a02c4d5b8d524404baa
Oreoluwa Aremu <aremu.oluwaranmilowo@gmail.com>
2022-10-18 02:58:39 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,13 +9,10 @@
 
 int main(void)
 {
-	int n;
+	int a;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
 	return (0);
 }
